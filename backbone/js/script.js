db8c8711f846b0695ca4e9734008ddcd3352a194
JoseOneTime <claveren@gmail.com>
2014-01-22 22:45:10 -0500

    Refactore backbone example and simulate network lag

--- backbone/js/script.js
+++ backbone/js/script.js
@@ -26,7 +26,7 @@ var router = Backbone.Router.extend({
 		this.homeView.render();
 	},
 	loadApple : function(appleName){
-		this.appleView.render(appleName);
+		this.appleView.loadApple(appleName);
 	}
 });
 
@@ -44,17 +44,39 @@ var homeView = Backbone.View.extend({
 var Apples = Backbone.Collection.extend({});
 
 var appleView = Backbone.View.extend({
+	initialize : function(){
+		// are the outer parentheses below really necessary?
+		this.model = new (Backbone.Model.extend({}));
+		this.model.bind('change', this.render, this);
+		this.bind('spinner', this.showSpinner, this);
+	},
 	template : _.template(
 		'<figure>' +
 		'<img src="<%= attributes.url %>"/>' +
 		'<figcaption><%= attributes.name %></figcaption>' +
 		'</figure>'
 		),
-	// TODO re-write with load apple and event binding
+	templateSpinner : '<img src="img/spinner.gif" width="30"/>',
+	loadApple : function(appleName){
+		this.trigger('spinner');
+		// show spinner GIF image
+		var view = this;
+		// we'll need to access that inside of a closure
+		// "this is just for scoping issues so we can access view within the code below"
+		setTimeout(function(){
+			// simulate real time lage when
+			// fetching data from the remote server
+			view.model.set(view.collection.where({
+				name : appleName
+			})[0].attributes);
+		}, 1000);
+	},
 	render : function(appleName) {
-		var appleModel = this.collection.where({ name : appleName })[0];
-		var appleHtml = this.template(appleModel);
+		var appleHtml = this.template(this.model);
 		$('body').html(appleHtml);
+	},
+	showSpinner : function(){
+		$('body').html(this.templateSpinner);
 	}
 });
 
